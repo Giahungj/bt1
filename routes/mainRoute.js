33b83087ff63101c82906a46935b74dccb8f9e90
Giahungj <giahng.n@gmail.com>
2024-11-13 02:49:18 +0700

    second commit

--- routes/mainRoute.js
+++ routes/mainRoute.js
@@ -2,11 +2,24 @@ import express from 'express';
 import { getHomePage } from '../controllers/homeController.js';
 import { getAboutPage } from '../controllers/aboutController.js';
 import { getContactPage } from '../controllers/contactController.js';
+import { get404Page } from '../controllers/404Controller.js';
+import { getNewUserPage } from '../controllers/UserController.js';
+import { getLoginPage } from '../controllers/UserController.js';
+import { authAccount } from '../controllers/UserController.js';
+import { createUser } from '../controllers/UserController.js';
 
 const router = express.Router();
 
 router.get('/', getHomePage);
 router.get('/about', getAboutPage);
 router.get('/contact', getContactPage);
+router.get('/create-new-user', getNewUserPage);
+router.get('/login', getLoginPage);
+router.get('/*', get404Page);
+
+router.post('/create-new-user', createUser);
+router.post('/login', authAccount);
+
+
 
 export default router;
